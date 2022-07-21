12831a18bf186110f8037627e0a08925776803b8
Asliddin-Ziyodullaev <a.ziyodullaev@student.inha.uz>
2022-07-21 16:12:08 +0500

    first commit

--- src/main/java/org/example/domain/auth/Answer.java
+++ src/main/java/org/example/domain/auth/Answer.java
@@ -1,6 +1,11 @@
 package org.example.domain.auth;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
 public class Answer {
+
+
     private Long id;
     private Long question_id;
     private String answer;
